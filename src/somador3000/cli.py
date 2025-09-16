15b3643c7c24bdcabceb83d6fa5d20f661c14abd
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2025-09-16 13:08:15 -0300

    Export methods soma and soma_3_mil

--- src/somador3000/cli.py
+++ src/somador3000/cli.py
@@ -1,11 +1,12 @@
 import click
+from .functions import soma_3_mil
 
 
 @click.command()
 @click.argument('numero', type=float)
 def main(numero):
     """Calcula o número passado mais 3000"""
-    resultado = numero + 3000
+    resultado = soma_3_mil(numero)
     click.echo(f"O resultado de {numero} + 3000 é: {resultado}")
 
 
